343ea6aa7a78e4b4aa11c29efdce2396c970c8dd
Amel <naloufi.amel@gmail.com>
2023-10-07 19:52:04 +0200

    avancement page d'accueil

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  index = 0;
+  slides!: HTMLCollectionOf<HTMLElement>;
+  total!: number;
 
+  constructor() { }
+
+  ngOnInit(): void {
+    this.slides = document.getElementsByClassName('slide') as HTMLCollectionOf<HTMLElement>;
+    this.total = this.slides.length;
+    this.showNextSlide();
+  }
+
+  showNextSlide() {
+    this.slides[this.index].style.display = 'none';
+
+    this.index++;
+
+    if (this.index === this.total) {
+      this.index = 0;
+    }
+
+    this.slides[this.index].style.display = 'block';
+
+    setTimeout(() => {
+      this.showNextSlide();
+    }, 2300);
+  }
 }
-
