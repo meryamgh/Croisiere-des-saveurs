a3c4716a6aeca9118dab8214d765634d5d686f35
Amel <naloufi.amel@gmail.com>
2023-10-07 22:35:31 +0200

    a la une

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { SlideComponent } from './slide/slide.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    SlideComponent
   ],
   imports: [
     BrowserModule,
