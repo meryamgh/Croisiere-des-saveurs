16ef92a892d10fa5803e6a5877f853cc0e8f59b4
Amel <naloufi.amel@gmail.com>
2023-10-07 23:08:33 +0200

    moitiee footer (accueil presque fini)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { SlideComponent } from './slide/slide.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    SlideComponent
+    SlideComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
