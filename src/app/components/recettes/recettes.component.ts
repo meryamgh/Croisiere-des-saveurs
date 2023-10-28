d0e9e4425eea3ba725aa4df60430a12543a4e99a
meryamgh <113671198+meryamgh@users.noreply.github.com>
2023-10-28 17:55:47 +0200

    nbr likes

--- src/app/components/recettes/recettes.component.ts
+++ src/app/components/recettes/recettes.component.ts
@@ -5,6 +5,7 @@ import {User} from "../../models/user.model";
 import {Favoris} from "../../models/favoris.model";
 import {FavorisService} from "../../services/favoris/favoris.service";
 import {recettes} from "../../data/data-loader-recettes";
+import {favoris} from "../../data/data-loader-favoris";
 
 
 @Component({
@@ -15,6 +16,7 @@ import {recettes} from "../../data/data-loader-recettes";
 export class RecettesComponent {
 
     public recettes : Map<string, string> = new Map<string, string>();
+    public recipeNbrLikes : Map<string, number> = new Map<string, number>();
     public recipes !: Recette[];
     public term!: string;
     public currentUser!: User;
@@ -23,18 +25,19 @@ export class RecettesComponent {
         this.loadData()
     }
 
-    public loadData(): void {
-
+    public getAllRecipes(){
         this.recetteService.getRecettes().subscribe(recettesData => {
             this.recipes = recettesData;
 
             recettesData.forEach((recette: Recette) => {
                 this.recettes.set(recette.nom, "false");
+                this.recipeNbrLikes.set(recette.nom, 0);
             });
         });
+    }
 
+    public getLikedRecipesByUser(){
         const storedUser = sessionStorage.getItem("userLogged");
-
         if (storedUser) {
             this.currentUser = JSON.parse(storedUser) as User;
 
@@ -45,25 +48,55 @@ export class RecettesComponent {
                 });
             });
         }
+    }
+
+    public getLikesByRecipes(){
+        this.favorisService.getFavoris().subscribe(data => {
+            data.forEach((favoris: Favoris) => {
+                console.log("ici")
+                const previousLikeNbr =  this.recipeNbrLikes.get(favoris.favoris);
+                console.log(previousLikeNbr)
+                if(previousLikeNbr!= undefined){
+                    console.log("ertghbvcx");
+                    this.recipeNbrLikes.set(favoris.favoris, previousLikeNbr+1);
+                }
+
+            })
+        })
+
+    }
+
+
+    public loadData(): void {
+
+         this.getAllRecipes();
+         this.getLikedRecipesByUser();
+        this.getLikesByRecipes();
 
     }
 
 
     getMap() {
     console.log(this.recettes);
+        console.log(this.recipeNbrLikes);
 
   }
 
     addFavoris(recetteClicked : Recette) {
         const recetteBoolean = this.recettes.get(recetteClicked.nom);
         const newFavoris = new Favoris(recetteClicked.nom,this.currentUser.email);
-        if(recetteBoolean === "true"){
-            this.recettes.set(recetteClicked.nom,"false");
-            this.favorisService.delFavoris(newFavoris).subscribe();
-        }else{
-            this.recettes.set(recetteClicked.nom,"true");
+        const nbrLikes = this.recipeNbrLikes.get(recetteClicked.nom);
+        if(nbrLikes!= undefined) {
+            if (recetteBoolean === "true") {
 
-            this.favorisService.addFavoris(newFavoris).subscribe();
+                this.recettes.set(recetteClicked.nom, "false");
+                this.recipeNbrLikes.set(recetteClicked.nom, nbrLikes - 1);
+                this.favorisService.delFavoris(newFavoris).subscribe();
+            } else {
+                this.recettes.set(recetteClicked.nom, "true");
+                this.recipeNbrLikes.set(recetteClicked.nom, nbrLikes + 1);
+                this.favorisService.addFavoris(newFavoris).subscribe();
+            }
         }
 
 
--- src/app/data/data-loader-favoris.ts
+++ src/app/data/data-loader-favoris.ts
@@ -5,10 +5,10 @@ import{users} from "./data-loader-user"
 export const favorisData = [
     {favoris: recettes[1].nom, user: users[1].email},
     {favoris: recettes[2].nom, user: users[1].email},
-    {favoris: recettes[2].nom, user: users[2].email},
-    {favoris: recettes[11].nom, user: users[2].email},
-    {favoris: recettes[12].nom, user: users[3].email},
-    {favoris: recettes[13].nom, user: users[2].email}
+    {favoris: recettes[2].nom, user: users[3].email},
+    {favoris: recettes[1].nom, user: users[4].email},
+    {favoris: recettes[5].nom, user: users[1].email},
+    {favoris: recettes[3].nom, user: users[2].email}
 ]
 
 export const favoris: Favoris[] = favorisData.map(favorisData => {
