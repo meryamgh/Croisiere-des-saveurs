fa8422fefe1a4f53490cbcee32b0bd5f148bc036
meryamgh <113671198+meryamgh@users.noreply.github.com>
2023-11-12 17:13:21 +0100

    inscription particles

--- src/app/components/inscription/inscription.component.ts
+++ src/app/components/inscription/inscription.component.ts
@@ -2,7 +2,9 @@ import {Component} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {UserService} from "../../services/user/user.service";
 import {Router} from "@angular/router";
-
+import {Container, Engine} from "tsparticles-engine";
+import {loadSlim} from "tsparticles-slim";
+import {particlesOptionsAnimation} from '../login/particles.config';
 
 @Component({
     selector: 'app-inscription',
@@ -18,6 +20,7 @@ export class InscriptionComponent {
         password: new FormControl('', [Validators.required, Validators.minLength(6)])
     });
     public userAlreadyExists!: boolean;
+    public particlesOptions = particlesOptionsAnimation;
 
     public constructor(private formBuilder: FormBuilder, private router: Router,
                 private userService: UserService) {
@@ -42,5 +45,12 @@ export class InscriptionComponent {
         });
     }
 
+    public async particlesInit(engine: Engine): Promise<void> {
+        const domArray: Container[] = engine.dom();
+        if (domArray.length > 0) {
+            domArray.splice(0, 1);
+        }
+        await loadSlim(engine);
+    }
 
 }
