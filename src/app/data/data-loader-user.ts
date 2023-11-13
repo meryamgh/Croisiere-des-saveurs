d87830fee01208e32f5f67ccac703aef828a3efd
meryamgh <113671198+meryamgh@users.noreply.github.com>
2023-11-13 19:58:04 +0100

    change branch

--- src/app/data/data-loader-user.ts
+++ src/app/data/data-loader-user.ts
@@ -4,35 +4,39 @@ import {faker} from "@faker-js/faker";
 
 export const users: User[] = [];
 
+
 const generateRandomUser = () => {
-    const user:User = {
-        nom: faker.person.lastName(),
-        prenom: faker.person.firstName(),
-        email: faker.internet.email(),
-        password: "test123",
-        highScore: Math.floor(Math.random() * 14) + 1,
-    };
+  const user: User = {
+    nom: faker.person.lastName(),
+    prenom: faker.person.firstName(),
+    email: faker.internet.email(),
+    password: "test123",
+    highScore: Math.floor(Math.random() * 14) + 1,
+    embarquementTicketNumber: `${Math.floor(Math.random() * (200 - 100 + 1)) + 100}${String.fromCharCode(Math.floor(Math.random() * 26) + 65)}`,
 
-    return user;
+
+  };
+
+  return user;
 };
 
-const generateRandomUsers = (count:number) => {
-    const randomUsers:User[] = [];
-    for (let i:number = 0; i < count; i++) {
-        const user:User = generateRandomUser();
-        randomUsers.push(user);
-    }
-    return randomUsers;
+const generateRandomUsers = (count: number) => {
+  const randomUsers: User[] = [];
+  for (let i: number = 0; i < count; i++) {
+    const user: User = generateRandomUser();
+    randomUsers.push(user);
+  }
+  return randomUsers;
 };
 
 
-users.push(...generateRandomUsers(20).map(userData => new User(userData.nom, userData.prenom, userData.email, userData.password,userData.highScore)));
+users.push(...generateRandomUsers(20).map(userData => new User(userData.nom, userData.prenom, userData.email, userData.password, userData.highScore, userData.embarquementTicketNumber)));
 
 users.push(
-    new User('Dupont', 'John', 'john@example.com', 'test123',9),
-    new User('Smith', 'Alice', 'alice@example.com', 'test123',3),
-    new User('Johnson', 'Michael', 'michael@example.com', 'test123',0),
-    new User('Lee', 'Emma', 'emma@example.com', 'test123',2),
-    new User('Kim', 'David', 'david@example.com', 'test123',8),
-    new User('Silva', 'Elyora', 'elyora@example.com', 'test123',28)
+  new User('Dupont', 'John', 'john@example.com', 'test123', 9, '18B'),
+  new User('Smith', 'Alice', 'alice@example.com', 'test123', 3, '45Q'),
+  new User('Johnson', 'Michael', 'michael@example.com', 'test123', 0, '541X'),
+  new User('Lee', 'Emma', 'emma@example.com', 'test123', 2, '93A'),
+  new User('Kim', 'David', 'david@example.com', 'test123', 8, '238I'),
+  new User('Silva', 'Elyora', 'elyora@example.com', 'test123', 28, '192H')
 );
--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -4,52 +4,55 @@ import {HttpClient} from '@angular/common/http';
 import {Observable, Subject} from "rxjs";
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class UserService {
-    private usersUrl: string = '/api/users/';
-    private userSubject: Subject<User | null> = new Subject<User | null>();
-    public userLogged !: User;
+  private usersUrl: string = '/api/users/';
+  private userSubject: Subject<User | null> = new Subject<User | null>();
+  public userLogged !: User;
 
-    public constructor(private http: HttpClient) {
-    }
-
-
-    public getUser(email: string): Observable<User> {
-        return this.http.get<User>(`${this.usersUrl}${email}`);
-    }
-
-    public createUser(user: User): Observable<Object> {
-        const jsonBody: string = JSON.stringify(user);
-        return this.http.post(this.usersUrl, jsonBody);
-    }
-
-    public updateUser(newUser: User): Observable<User> {
-        const jsonBody: string = JSON.stringify(newUser);
-        return this.http.put<User>(this.usersUrl, jsonBody);
-    }
-
-    public getUserWithHighestScore(): Observable<User> {
-        return this.http.get<User>(this.usersUrl + 'highestScore');
-    }
+  public constructor(private http: HttpClient) {
+  }
 
 
-    public isLogged(userLoggedId: User): void {
-        sessionStorage.setItem("userLogged", JSON.stringify(userLoggedId));
-        this.userSubject.next(userLoggedId);
-        this.userLogged = userLoggedId;
-    }
+  public getUser(email: string): Observable<User> {
+    return this.http.get<User>(`${this.usersUrl}${email}`);
+  }
 
-    public getDeconnected(): void {
-        sessionStorage.removeItem("userLogged");
-        this.userSubject.next(null);
-    }
+  public createUser(user: User): Observable<Object> {
+    user.embarquementTicketNumber = `${Math.floor(Math.random() * (200 - 100 + 1)) + 100}${String.fromCharCode(Math.floor(Math.random() * 26) + 65)}`;
+    user.highScore = 0;
+    const jsonBody: string = JSON.stringify(user);
 
-    public getUserSubject(): Subject<User | null> {
-        return this.userSubject;
-    }
+    return this.http.post(this.usersUrl, jsonBody);
+  }
 
-    public setUserSubject(newUser: User): void {
-        this.userSubject.next(newUser);
-    }
+  public updateUser(newUser: User): Observable<User> {
+    const jsonBody: string = JSON.stringify(newUser);
+    return this.http.put<User>(this.usersUrl, jsonBody);
+  }
+
+  public getUserWithHighestScore(): Observable<User> {
+    return this.http.get<User>(this.usersUrl + 'highestScore');
+  }
+
+
+  public isLogged(userLoggedId: User): void {
+    sessionStorage.setItem("userLogged", JSON.stringify(userLoggedId));
+    this.userSubject.next(userLoggedId);
+    this.userLogged = userLoggedId;
+  }
+
+  public getDeconnected(): void {
+    sessionStorage.removeItem("userLogged");
+    this.userSubject.next(null);
+  }
+
+  public getUserSubject(): Subject<User | null> {
+    return this.userSubject;
+  }
+
+  public setUserSubject(newUser: User): void {
+    this.userSubject.next(newUser);
+  }
 }
