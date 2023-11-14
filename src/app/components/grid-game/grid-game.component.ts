0f3ec61535fbb4155da78f2004143737b4982d72
meryamgh <113671198+meryamgh@users.noreply.github.com>
2023-11-14 14:12:18 +0100

    map update

--- src/app/components/grid-game/grid-game.component.ts
+++ src/app/components/grid-game/grid-game.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-grid-game',
@@ -6,32 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./grid-game.component.scss']
 })
 export class GridGameComponent {
-  public popupOuverte:boolean = false;
+  public popupOuverte: boolean = false;
 
+  constructor(private renderer: Renderer2) {}
 
-  public ouvrirPopup():void {
+  public ouvrirPopup(): void {
     this.popupOuverte = true;
     this.lockScroll();
   }
 
-  public fermerPopup():void {
+  public fermerPopup(): void {
     this.popupOuverte = false;
     this.unlockScroll();
   }
+
   lockScroll(): void {
     const body = document.body;
-
-    body.style.position = 'fixed';
-    body.style.top = '0px';
-    body.style.left = '0px';
-    body.style.right = '0px';
-
+    this.renderer.setStyle(body, 'position', 'fixed');
+    this.renderer.setStyle(body, 'top', '0px');
+    this.renderer.setStyle(body, 'left', '0px');
+    this.renderer.setStyle(body, 'right', '0px');
   }
 
   unlockScroll(): void {
     const body = document.body;
-    body.style.position = '';
-    body.style.top = '';
-
+    this.renderer.removeStyle(body, 'position');
+    this.renderer.removeStyle(body, 'top');
   }
 }
--- src/app/services/map/map.service.ts
+++ src/app/services/map/map.service.ts
@@ -11,7 +11,7 @@ export class MapService {
     private map!: any;
     private readonly TILE_LAYER_URL: string = 'https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png';
     private readonly FLAGS_FOLDER: string = 'assets/flags/';
-
+    private animatedCircles: L.Circle[] = [];
 
     public constructor(private router: Router, private countryService: CountryService) {
     }
@@ -48,19 +48,90 @@ export class MapService {
             ],
             minZoom: 2,
             maxZoom: 18,
-            dragging: false,
         });
 
         const tiles = L.tileLayer(this.TILE_LAYER_URL, {
             subdomains: 'abcd',
-            noWrap: true,
             maxZoom: 20
         });
 
         tiles.addTo(this.map);
         this.setCountries();
-
+        this.animationsMap();
 
     }
+
+    public animationsMap(){
+
+      const c1=  L.circle([0,0], { // Spread operator to convert to LatLngTuple
+          color: 'blue',
+          fillColor: 'blue',
+          fillOpacity: 0.5,
+          radius: 100000,  // Example radius, adjust as needed
+        }).addTo(this.map);
+
+      const c2=  L.circle([0,30], { // Spread operator to convert to LatLngTuple
+        color: 'blue',
+        fillColor: 'blue',
+        fillOpacity: 0.5,
+        radius: 100000,  // Example radius, adjust as needed
+      }).addTo(this.map);
+
+      const c3=   L.circle([0,-30], { // Spread operator to convert to LatLngTuple
+        color: 'blue',
+        fillColor: 'blue',
+        fillOpacity: 0.5,
+        radius: 100000,  // Example radius, adjust as needed
+      }).addTo(this.map);
+
+      const c4=  L.circle([30,0], { // Spread operator to convert to LatLngTuple
+        color: 'blue',
+        fillColor: 'blue',
+        fillOpacity: 0.5,
+        radius: 100000,  // Example radius, adjust as needed
+      }).addTo(this.map);
+
+      const c5=   L.circle([-30,0], { // Spread operator to convert to LatLngTuple
+        color: 'blue',
+        fillColor: 'blue',
+        fillOpacity: 0.5,
+        radius: 100000,  // Example radius, adjust as needed
+      }).addTo(this.map);
+
+      const c6=  L.circle([60,0], { // Spread operator to convert to LatLngTuple
+        color: 'blue',
+        fillColor: 'blue',
+        fillOpacity: 0.5,
+        radius: 100000,  // Example radius, adjust as needed
+      }).addTo(this.map);
+
+      const c7=   L.circle([-60,0], { // Spread operator to convert to LatLngTuple
+        color: 'blue',
+        fillColor: 'blue',
+        fillOpacity: 0.5,
+        radius: 100000,  // Example radius, adjust as needed
+      }).addTo(this.map);
+
+      this.animatedCircles.push(c1);
+      this.animatedCircles.push(c2);
+      this.animatedCircles.push(c3);
+      this.animatedCircles.push(c4);
+      this.animatedCircles.push(c5);
+      this.animatedCircles.push(c6);
+
+      setInterval(() => {
+        this.animatedCircles.forEach((circle) => {
+          const currentRadius = circle.getRadius();
+          const newRadius = this.getRandomRadius();
+          circle.setRadius(newRadius);
+        });
+      }, 1000); // Update every second, adjust as needed
+    }
+
+  private getRandomRadius(): number {
+    const minRadius = 50000;
+    const maxRadius = 100000;
+    return Math.floor(Math.random() * (maxRadius - minRadius + 1)) + minRadius;
+  }
 
 }
