cddf48c455d6f44aead8ca124265a8e55b316eb7
meryamgh <113671198+meryamgh@users.noreply.github.com>
2023-11-14 14:13:54 +0100

    clean map

--- src/app/services/map/map.service.ts
+++ src/app/services/map/map.service.ts
@@ -57,81 +57,10 @@ export class MapService {
 
         tiles.addTo(this.map);
         this.setCountries();
-        this.animationsMap();
+
 
     }
 
-    public animationsMap(){
 
-      const c1=  L.circle([0,0], { // Spread operator to convert to LatLngTuple
-          color: 'blue',
-          fillColor: 'blue',
-          fillOpacity: 0.5,
-          radius: 100000,  // Example radius, adjust as needed
-        }).addTo(this.map);
-
-      const c2=  L.circle([0,30], { // Spread operator to convert to LatLngTuple
-        color: 'blue',
-        fillColor: 'blue',
-        fillOpacity: 0.5,
-        radius: 100000,  // Example radius, adjust as needed
-      }).addTo(this.map);
-
-      const c3=   L.circle([0,-30], { // Spread operator to convert to LatLngTuple
-        color: 'blue',
-        fillColor: 'blue',
-        fillOpacity: 0.5,
-        radius: 100000,  // Example radius, adjust as needed
-      }).addTo(this.map);
-
-      const c4=  L.circle([30,0], { // Spread operator to convert to LatLngTuple
-        color: 'blue',
-        fillColor: 'blue',
-        fillOpacity: 0.5,
-        radius: 100000,  // Example radius, adjust as needed
-      }).addTo(this.map);
-
-      const c5=   L.circle([-30,0], { // Spread operator to convert to LatLngTuple
-        color: 'blue',
-        fillColor: 'blue',
-        fillOpacity: 0.5,
-        radius: 100000,  // Example radius, adjust as needed
-      }).addTo(this.map);
-
-      const c6=  L.circle([60,0], { // Spread operator to convert to LatLngTuple
-        color: 'blue',
-        fillColor: 'blue',
-        fillOpacity: 0.5,
-        radius: 100000,  // Example radius, adjust as needed
-      }).addTo(this.map);
-
-      const c7=   L.circle([-60,0], { // Spread operator to convert to LatLngTuple
-        color: 'blue',
-        fillColor: 'blue',
-        fillOpacity: 0.5,
-        radius: 100000,  // Example radius, adjust as needed
-      }).addTo(this.map);
-
-      this.animatedCircles.push(c1);
-      this.animatedCircles.push(c2);
-      this.animatedCircles.push(c3);
-      this.animatedCircles.push(c4);
-      this.animatedCircles.push(c5);
-      this.animatedCircles.push(c6);
-
-      setInterval(() => {
-        this.animatedCircles.forEach((circle) => {
-          const currentRadius = circle.getRadius();
-          const newRadius = this.getRandomRadius();
-          circle.setRadius(newRadius);
-        });
-      }, 1000); // Update every second, adjust as needed
-    }
-
-  private getRandomRadius(): number {
-    const minRadius = 50000;
-    const maxRadius = 100000;
-    return Math.floor(Math.random() * (maxRadius - minRadius + 1)) + minRadius;
-  }
 
 }
