a15555f0199c0e3924cc9a052d532d6d5c182950
Amel <naloufi.amel@gmail.com>
2023-11-14 15:18:25 +0100

    facto

--- src/app/components/recette/recette.component.ts
+++ src/app/components/recette/recette.component.ts
@@ -11,158 +11,155 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 
 @Component({
-    selector: 'app-recette',
-    templateUrl: './recette.component.html',
-    styleUrls: ['./recette.component.scss']
+  selector: 'app-recette',
+  templateUrl: './recette.component.html',
+  styleUrls: ['./recette.component.scss']
 })
 export class RecetteComponent implements OnInit {
-    public currentReciepie !: Recette;
-    public currentUser!: User;
-    public commentaires!: Commentaire[];
-    public nbrFav !: number;
-    public recetteFav: boolean = false;
-    public etapesPreparationPopupOpen: boolean = false;
+  public currentReciepie !: Recette;
+  public currentUser!: User;
+  public commentaires!: Commentaire[];
+  public nbrFav !: number;
+  public recetteFav: boolean = false;
+  public etapesPreparationPopupOpen: boolean = false;
 
-    public usersComments: Map<string, string> = new Map<string, string>;
-    public commentaireForm: FormGroup = this.fb.group({
-        commentaire: ['', Validators.required]
+  public usersComments: Map<string, string> = new Map<string, string>;
+  public commentaireForm: FormGroup = this.fb.group({
+    commentaire: ['', Validators.required]
+  });
+
+  public constructor(private recetteService: RecetteService, private commentaireService: CommentaireService,
+                     private route: ActivatedRoute, private router: Router,
+                     private favorisService: FavorisService,
+                     private fb: FormBuilder) {
+  }
+
+  public getReciepie(): void {
+
+    this.route.params.subscribe(params => {
+      const recetteId = params['nom'];
+      this.recetteService.getRecette(recetteId).subscribe(data => {
+        this.currentReciepie = data;
+        this.getNbrFavRecette();
+
+      });
+    });
+  }
+
+  public getComments(): void {
+
+    this.commentaireService.getCommentsRecipie(this.currentReciepie.nom).subscribe(data => {
+      this.commentaires = data;
+
+      data.forEach((comment: Commentaire) => {
+
+        if (this.currentUser) {
+          if (comment.user === this.currentUser.email) {
+            this.usersComments.set(comment.user, "user-logged");
+          } else {
+            this.usersComments.set(comment.user, "user-notLogged");
+          }
+        } else {
+          this.usersComments.set(comment.user, "user-notLogged");
+        }
+
+      });
     });
 
-    public constructor(private recetteService: RecetteService, private commentaireService: CommentaireService,
-                       private route: ActivatedRoute, private router: Router,
-                       private favorisService: FavorisService,
-                       private fb: FormBuilder) {
+  }
+
+  public getNbrFavRecette(): void {
+    this.favorisService.getFavorisRecette(this.currentReciepie.nom).subscribe(data =>
+      (this.nbrFav = data.length)
+    )
+    if (this.currentUser) {
+      this.favorisService.getFavorisUserRecette(this.currentUser.email, this.currentReciepie.nom).subscribe(data => {
+          this.recetteFav = data.length !== 0;
+        }
+      )
+    }
+  }
+
+
+  public getUser(): void {
+    const storedUser: string | null = sessionStorage.getItem("userLogged");
+
+    if (storedUser) {
+      this.currentUser = JSON.parse(storedUser) as User;
+    }
+  }
+
+  public addToFav(): void {
+    if (this.currentUser) {
+
+      const newFavoris: Favoris = new Favoris(this.currentReciepie.nom, this.currentUser.email, this.currentReciepie.picture);
+      if (this.recetteFav) {
+        this.nbrFav--;
+        this.favorisService.delFavoris(newFavoris).subscribe();
+      } else {
+        this.nbrFav++;
+        this.favorisService.addFavoris(newFavoris).subscribe();
+      }
+      this.recetteFav = !this.recetteFav;
+    } else {
+      this.router.navigate(['/login']);
     }
 
-    public getReciepie(): void {
+  }
 
-        this.route.params.subscribe(params => {
-            const recetteId = params['nom'];
-            this.recetteService.getRecette(recetteId).subscribe(data => {
-                this.currentReciepie = data;
-                this.getNbrFavRecette();
+  public deleteCommentaireSend(commentId: number): void {
+    this.commentaireService.deleteComment(commentId).subscribe(() => {
+      this.commentaires = this.commentaires.filter(
+        (comment: Commentaire): boolean => comment.idCommentaire !== commentId
+      );
+    });
+  }
 
-            });
+  public ajoutCommentaire(): void {
+
+    if (this.commentaireForm.valid) {
+      if (this.currentUser) {
+        const commentaire = this.commentaireForm.get('commentaire')?.value;
+        const commentToAdd = new Commentaire(commentaire, this.currentUser.email,
+          this.currentReciepie.nom, new Date());
+        this.usersComments.set(this.currentUser.email, "user-logged");
+        this.commentaireService.addComment(commentToAdd).subscribe();
+        this.commentaires.push(commentToAdd);
+        this.commentaireForm.reset();
+
+
+      } else {
+        this.router.navigate(['/login']);
+      }
+    }
+
+  }
+
+  ngOnInit(): void {
+    this.getReciepie();
+    this.getUser();
+  }
+
+  public updateCommentaire(commentToUpdate: Commentaire): void {
+    this.commentaireService.updateComment(commentToUpdate.idCommentaire, commentToUpdate.commentaire)
+      .subscribe((updatedComment: Commentaire) => {
+        this.commentaires = this.commentaires.map((comment) => {
+          if (comment.idCommentaire === commentToUpdate.idCommentaire) {
+            return updatedComment;
+          }
+          return comment;
         });
-    }
 
-    public getComments(): void {
+      });
 
-        this.commentaireService.getCommentsRecipie(this.currentReciepie.nom).subscribe(data => {
-            this.commentaires = data;
+  }
 
-            data.forEach((comment: Commentaire) => {
+  public openPreparationPopup(): void {
+    this.etapesPreparationPopupOpen = true;
+  }
 
-                if (this.currentUser) {
-                    if (comment.user === this.currentUser.email) {
-                        this.usersComments.set(comment.user, "user-logged");
-                    } else {
-                        this.usersComments.set(comment.user, "user-notLogged");
-                    }
-                } else {
-                    this.usersComments.set(comment.user, "user-notLogged");
-                }
-
-            });
-        });
-
-    }
-
-
-    public getNbrFavRecette(): void {
-        this.favorisService.getFavorisRecette(this.currentReciepie.nom).subscribe(data =>
-            (this.nbrFav = data.length)
-        )
-        if (this.currentUser) {
-            this.favorisService.getFavorisUserRecette(this.currentUser.email, this.currentReciepie.nom).subscribe(data => {
-                    this.recetteFav = data.length !== 0;
-                }
-            )
-        }
-    }
-
-
-    public getUser(): void {
-        const storedUser:string|null = sessionStorage.getItem("userLogged");
-
-        if (storedUser) {
-            this.currentUser = JSON.parse(storedUser) as User;
-        }
-    }
-
-
-    public addToFav(): void {
-        if (this.currentUser) {
-
-            const newFavoris:Favoris = new Favoris(this.currentReciepie.nom, this.currentUser.email, this.currentReciepie.picture);
-            if (this.recetteFav) {
-                this.nbrFav--;
-                this.favorisService.delFavoris(newFavoris).subscribe();
-            } else {
-                this.nbrFav++;
-                this.favorisService.addFavoris(newFavoris).subscribe();
-            }
-            this.recetteFav = !this.recetteFav;
-        } else {
-            this.router.navigate(['/login']);
-        }
-
-    }
-
-    public deleteCommentaireSend(commentId: number): void {
-        this.commentaireService.deleteComment(commentId).subscribe(() => {
-            this.commentaires = this.commentaires.filter(
-                (comment: Commentaire): boolean => comment.idCommentaire !== commentId
-            );
-        });
-    }
-
-    public ajoutCommentaire(): void {
-
-        if (this.commentaireForm.valid) {
-            if (this.currentUser) {
-                const commentaire = this.commentaireForm.get('commentaire')?.value;
-                const commentToAdd = new Commentaire(commentaire, this.currentUser.email,
-                    this.currentReciepie.nom, new Date());
-                this.usersComments.set(this.currentUser.email, "user-logged");
-                this.commentaireService.addComment(commentToAdd).subscribe();
-                this.commentaires.push(commentToAdd);
-                this.commentaireForm.reset();
-
-
-            } else {
-                this.router.navigate(['/login']);
-            }
-        }
-
-    }
-
-    ngOnInit(): void {
-        this.getReciepie();
-        this.getUser();
-    }
-
-    public updateCommentaire(commentToUpdate: Commentaire): void {
-        this.commentaireService.updateComment(commentToUpdate.idCommentaire, commentToUpdate.commentaire)
-            .subscribe((updatedComment: Commentaire) => {
-                this.commentaires = this.commentaires.map((comment) => {
-                    if (comment.idCommentaire === commentToUpdate.idCommentaire) {
-                        return updatedComment;
-                    }
-                    return comment;
-                });
-
-            });
-
-
-    }
-
-    public openPreparationPopup():void {
-        this.etapesPreparationPopupOpen = true;
-    }
-
-    public fermerPreparationPopup():void {
-        this.etapesPreparationPopupOpen = false;
-    }
+  public fermerPreparationPopup(): void {
+    this.etapesPreparationPopupOpen = false;
+  }
 
 }
